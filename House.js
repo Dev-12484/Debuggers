a84fbb20f81607c4426e28ec78d73ce31777b428
Dev-12484 <83420887+Dev-12484@users.noreply.github.com>
2021-09-12 16:54:44 +0400

    Add files via upload

---
+++ House.js
@@ -0,0 +1,29 @@
+function edit(){
+document.getElementById("start").style.display="none";
+document.getElementById("oute").style.display="block";
+}
+
+function ttos(){
+document.getElementById("oute").style.display="none";
+document.getElementById("outer").style.display="block";
+}
+
+function stop(){
+document.getElementById("outer").style.display="none";
+document.getElementById("out").style.display="block";
+}
+
+function toteach(){
+document.getElementById("outer").style.display="none";
+document.getElementById("oute").style.display="block";
+}
+
+function tostart(){
+document.getElementById("oute").style.display="none";
+document.getElementById("start").style.display="block";
+}
+
+function tostud(){
+document.getElementById("out").style.display="none";
+document.getElementById("outer").style.display="block";
+}
